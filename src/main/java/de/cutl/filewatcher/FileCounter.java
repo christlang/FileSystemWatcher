3998ff35719b40e1848c6a6b0c88df83a9c904c7
lange <lange@synyx.de>
2017-06-15 08:15:15 +0200

    created FileCounter

--- src/main/java/de/cutl/filewatcher/FileCounter.java
+++ src/main/java/de/cutl/filewatcher/FileCounter.java
@@ -1,8 +1,28 @@
 package de.cutl.filewatcher;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
- * Counting files recursive in path
+ * all file IO.
  */
 public class FileCounter {
 
+    /**
+     * Counting files in all Subfolders.
+     *
+     * @param dir
+     * @return count of all files
+     * @throws IOException
+     */
+    public int count(File dir) throws IOException {
+        final AtomicInteger fileCount = new AtomicInteger();
+        Files.list(Paths.get(dir.getAbsolutePath())).forEach((file) -> {
+            fileCount.incrementAndGet();
+        });
+        return fileCount.get();
+    }
 }
--- src/test/java/de/cutl/filewatcher/FileCounterTest.java
+++ src/test/java/de/cutl/filewatcher/FileCounterTest.java
@@ -1,31 +1,79 @@
 package de.cutl.filewatcher;
 
 import junit.framework.TestCase;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileCounterTest extends TestCase {
 
-    private Path TEST_DIR;
+
+
+    @Rule
+    public TemporaryFolder TEST_DIR = new TemporaryFolder(new File("target"));
+
+    private FileCounter cut;
 
     @Before
     public void setUp() throws IOException {
-        TEST_DIR = FileSystems.getDefault().getPath("target", "randome");
+        TEST_DIR.create();
 
-        if (Files.exists(TEST_DIR)) {
-            Files.delete(TEST_DIR);
-        }
-        Files.createDirectory(TEST_DIR);
+        cut = new FileCounter();
+    }
+
+    @After
+    public void tearDown() {
+        //TEST_DIR.delete();
+    }
+
+
+    @Test
+    public void testEmptyDirectory() throws IOException {
+        int result = cut.count(TEST_DIR.getRoot());
+
+        assertEquals(0, result);
     }
 
     @Test
-    public void testFileCountingInPath() throws IOException {
+    public void testDirectoryOneFile() throws IOException {
+        TEST_DIR.newFile("test.txt");
 
+        assertEquals(1, cut.count(TEST_DIR.getRoot()));
+    }
 
+    @Test
+    public void testDirectoryTwoFile() throws IOException {
+        TEST_DIR.newFile("test1.txt");
+        TEST_DIR.newFile("test2.txt");
+
+        assertEquals(2, cut.count(TEST_DIR.getRoot()));
+    }
+
+    @Test
+    public void testDirectoryOneFileInSubdir() throws IOException {
+        Path subDir = Files.createDirectory(Paths.get(TEST_DIR.getRoot().getAbsolutePath(), "subdir"));
+
+        Files.createFile(Paths.get(subDir.toString(), "test.txt"));
+
+        assertEquals(1, cut.count(TEST_DIR.getRoot()));
+    }
+
+    @Test
+    public void testDirectoryOneFileInSubdirAndOneFile() throws IOException {
+        Path subDir = Files.createDirectory(Paths.get(TEST_DIR.getRoot().getAbsolutePath(), "subdir"));
+
+        Files.createFile(Paths.get(subDir.toString(), "test.txt"));
+        TEST_DIR.newFile("test1.txt");
+
+        assertEquals(2, cut.count(TEST_DIR.getRoot()));
     }
 }
